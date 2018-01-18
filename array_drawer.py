5f87baaa3048c0a945ac68e8d8e0473550e2149d
KiRtAp2 <posta.patrik@gmail.com>
2018-01-18 18:14:25 +0100

    array drawer

--- array_drawer.py
+++ array_drawer.py
@@ -1,28 +1,28 @@
 import turtle
 
-def preglednica(dolzina, nx, ny):
-    wy = dolzina/ny
-    wx = dolzina/nx
+def array(length, nx, ny):
+    wy = length/ny
+    wx = length/nx
     
     t = turtle.Turtle()
     t.penup()
     t.setpos(-350,350)
     t.pendown()
     
-    t.forward(dolzina)
+    t.forward(length)
     t.right(90)
-    t.forward(dolzina)
+    t.forward(length)
     t.right(90)
-    t.forward(dolzina)
+    t.forward(length)
     t.right(90)
-    t.forward(dolzina)
+    t.forward(length)
     t.right(90)
 
     for _ in range(nx-1):
         t.forward(wx)
         t.right(90)
-        t.forward(dolzina)
-        t.back(dolzina)
+        t.forward(length)
+        t.back(length)
         t.left(90)
 
     t.forward(wx)
@@ -31,8 +31,8 @@ def preglednica(dolzina, nx, ny):
     for _ in range(ny-1):
         t.forward(wy)
         t.right(90)
-        t.forward(dolzina)
-        t.back(dolzina)
+        t.forward(length)
+        t.back(length)
         t.left(90)
 
     t.forward(wy)
@@ -40,4 +40,4 @@ def preglednica(dolzina, nx, ny):
     t.forward(500)
         
 
-preglednica(350, 4, 4)
+array(350, 4, 4)
