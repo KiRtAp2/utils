18d3e75ccb99d95e58dd073a0e09b8bfbe3587ad
No <posta.patrik@gmail.com>
2018-07-26 12:56:24 +0200

    Added comments

--- searchweb.py
+++ searchweb.py
@@ -7,58 +7,64 @@ import requests
 import re
 
 
+# Parts of the original URL
 netloc = None
 scheme = None
 scheme_netloc_sep = None
+
+# The search query
 query = None
-curr_site = None
+
 seen_sites = set()
 
 
 class Parser(HTMLParser):
     
     def handle_starttag(self, tag, attrs):
+        # When we get an <a> tag, we will go deeper into the search tree - if it's linking to the same site
         if tag == "a":
             for att in attrs:
                 
-                if att[0] != "href": 
+                # We're only interested in links, not classes or other attributes
+                if att[0] != "href":
                     continue
 
+                # Internal links on a webpage should be relative
+                # Here, we get rid of any absolute links (hopefully)
                 if re.match(r"^http(s)?://", att[1]):
                     continue
 
+                # We don't really want to mail people in this app
                 if att[1].startswith("mailto:"):
                     continue
 
-                if re.match(r".+\.[a-zA-Z]{2,4}$", att[1]):  # we do not want to look at pdfs, pictures etc.
-                    if not (att[1].endswith(".html") or att[1].endswith(".htm")): # we do, however, want to look at html data
-                        #print("regex on {} matched. Skipping.".format(att[1]))
+                # we do not want to look at pdfs, pictures etc.
+                if re.match(r".+\.[a-zA-Z]{2,4}$", att[1]):
+
+                    # we do, however, want to look at html pages
+                    if not (att[1].endswith(".html") or att[1].endswith(".htm")): 
                         continue
 
+                # We don't want to follow id links
                 if "#" in att[1]:
-                    # Strip it
                     idx = att[1].find("#")
                     att = att[0], att[1][:idx]
 
+                # Some addresses have a preceding and succeding \' for some reason
                 if att[1].startswith("\\'") and att[1].endswith("\\'"):
                     s = att[1][2:-2]
                     att = att[0], s
-
-                # print("Found subdomain: {}".format(att[1]))
                 
+                # sometimes, these strippings cause us to end up with an empty string
                 if len(att[1]) < 1:
                     continue
 
-                # print(scheme, scheme_netloc_sep, netloc, att[1])
                 if att[1][0] == "/":
                     new_site = scheme + scheme_netloc_sep + netloc + att[1]
                 else:
                     new_site = scheme + scheme_netloc_sep + netloc + "/" + att[1]
 
                 scan_site(new_site)
-
-    def handle_endtag(self, tag):
-        pass
 
     def handle_data(self, data):
         if query in data.lower():
@@ -67,13 +73,9 @@ class Parser(HTMLParser):
 
 def scan_site(url):
     global curr_site
-    #print("Scanning {}...".format(url), end="")
     if url in seen_sites:
-        #print("Already seen.")
         return
-    #print()
 
-    curr_site = url
     seen_sites.add(url)
 
     pars = Parser()
@@ -81,7 +83,7 @@ def scan_site(url):
     try:
         html = req.content.decode("UTF-8")
     except UnicodeDecodeError:
-        #print("Could not decode data from {}. It may be an image or document".format(url), file=stderr)
+        # If the website gives us pictures that don't end in an extension
         return
     pars.feed(req.content.decode("UTF-8"))
 
@@ -112,8 +114,12 @@ def main():
     
     query = " ".join(argv[2:]).lower()
     print('Scanning {} for "{}". This may take a while ...'.format(argv[1], query))
+    start_time = time.time()
+    
     scan_site(argv[1])
 
+    end_time = time.time()
+    print("Done! Took {} seconds.".format(end_time-start_time))
 
 
 if __name__ == "__main__":
